5e7b491652792a581271d8afb54f617a35a3c15c
zw <184554365@qq.com>
2021-05-04 15:39:53 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
   <el-container>
     <el-aside width="250px" style="height: 100vh">
-      <div>
-        <div class="step-card">
-          <div>初始投资</div>
+      <div
+        v-for="(cardItem, cardIndex) in tradingPerviewList"
+        :key="'card' + cardIndex"
+      >
+        <div
+          :class="
+            perviewChooseIndex == cardIndex ? 'step-card-choose' : 'step-card'
+          "
+        >
+          <div v-if="cardIndex == 0">初始投资</div>
+          <div v-else>第{{ cardIndex }}次投资</div>
           <el-row class="margin-top">
             <el-col :span="12">
               <div class="text-sm">投资建议</div>
-              <div class="text-bold">买入1%</div>
+              <div class="text-bold">{{ cardItem.suggest }}</div>
             </el-col>
             <el-col :span="12">
               <div class="text-sm">投资结果</div>
-              <div class="text-bold">等待计算</div>
-            </el-col>
-          </el-row>
-        </div>
-        <div class="step-card-choose margin-top">
-          <div>第1次投资</div>
-          <el-row class="margin-top">
-            <el-col :span="12">
-              <div class="text-sm">投资建议</div>
-              <div class="text-bold">等待计算</div>
-            </el-col>
-            <el-col :span="12">
-              <div class="text-sm">投资结果</div>
-              <div class="text-bold">等待计算</div>
+              <div class="text-bold">{{ cardItem.result }}</div>
             </el-col>
           </el-row>
         </div>
@@ -43,7 +38,7 @@
               <div class="text-sm">累计收益率</div>
             </el-col>
             <el-col class="detail-box" :span="8">
-              <div class="text-xl text-bold">2</div>
+              <div class="text-xl text-bold">{{ winTimes }}</div>
               <div class="text-sm">盈利次数</div>
             </el-col>
           </el-row>
@@ -51,7 +46,7 @@
             <el-col>
               <div>
                 <el-input
-                  placeholder="请输入本轮的涨跌幅，例如：-15（-15%）"
+                  placeholder="请输入本轮的涨跌幅,例如：-15（-15%）"
                   v-model="input2"
                 >
                   <template v-slot:append>提交计算</template>
@@ -97,17 +92,8 @@
           <!-- 策略解读 -->
           <div class="trading-desc margin-top-lg">
             <div>策略解读：</div>
-            <!-- 算法a结果数量 > 0,执行算法b -->
             <div class="margin-top">
-              在所有可能的决策中包含了大概率保本的决策，选择其中最大化预期收益
-            </div>
-            <!-- 算法a结果数量 == 0, 且平均涨跌幅 > 0 -->
-            <div class="margin-top">
-              在所有可能的决策中不包含大概率保本的决策，但是平均涨跌幅大于0，还是有概率能回本，所以调整仓位在预期平均涨幅的情况下最接近可以回本的比例
-            </div>
-            <!-- 算法a结果数量 == 0, 且平均涨跌幅 <= 0 -->
-            <div class="margin-top">
-              在所有可能的决策中不包含大概率保本的决策，且平均涨跌幅小于0，无法回本的概率较大，加大投入或者减少投入都有可能造成不可挽回的损失，所以暂时不调整仓位
+              {{ decisionReason[0] }}
             </div>
           </div>
         </el-col>
@@ -120,18 +106,144 @@
 import { ref } from "vue";
 // 盈利次数
 let winTimes = ref(0);
-// 左手边的投资预览 [本轮涨跌幅，改变建议，投资结果，是否已提交计算，是否已确认操作]
-let tradingPerviewList = ref([]);
-// 每轮的投资结果 [涨跌幅，总价值，累计本金，净收益，可买金额，可卖金额]
-let tradingDetailList = ref([]);
-// 每轮的投资选择 {chooseIndex: 0, reason:""}
-let tradingChooseList = ref([]);
+// 选中预览
+let perviewChooseIndex = ref(1);
+// 左手边的投资预览 {本轮涨跌幅,改变建议,投资结果,是否已提交计算,是否已确认操作}
+let tradingPerviewList = ref([
+  {
+    vol: 0,
+    suggest: "买入1%",
+    result: "等待计算",
+    isCalc: true,
+    isConfirm: true,
+  },
+  {
+    vol: 0,
+    suggest: "等待计算",
+    result: "等待计算",
+    isCalc: false,
+    isConfirm: false,
+  },
+]);
+// 每轮的投资结果 {涨跌幅,总价值,累计本金,累计收益,可买金额,可卖金额}
+let tradingDetailList = ref([
+  {
+    vol: 0,
+    totalValue: 10000,
+    totalCap: 10000,
+    netValue: 10000,
+    buy: 10000,
+    sell: 0,
+  },
+]);
+// 每轮的投资选择 {chooseIndex: 0, reason: 1}
+let tradingChooseList = ref([
+  {
+    chooseIndex: 0,
+    reason: 1,
+  },
+]);
+// 决策解释
+const decisionReason = [
+  // 算法a结果数量 > 0,执行算法b
+  "在所有可能的决策中包含了大概率保本的决策，选择其中最大化预期收益",
+  // 算法a结果数量 == 0, 且平均涨跌幅 > 0
+  "在所有可能的决策中不包含大概率保本的决策，但是平均涨跌幅大于0，还是有概率能回本，所以调整仓位在预期平均涨幅的情况下最接近可以回本的比例",
+  // 算法a结果数量 == 0, 且平均涨跌幅 <= 0
+  "在所有可能的决策中不包含大概率保本的决策，且平均涨跌幅小于0，无法回本的概率较大，加大投入或者减少投入都有可能造成不可挽回的损失，所以暂时不调整仓位",
+];
 // 投资结果模板 买入1%-100%、卖出1%-100%、不操作
-// 标题，交易类型，交易比例，最大预期跌幅结果，最大跌幅结果减本金的差，回本需要的最小预期涨幅
+// 标题,交易类型,交易比例,最大预期跌幅结果,最大跌幅结果减本金的差,回本需要的最小预期涨幅
 // {title:"买入1%",tradingType:"buy",rate:0.01,maxFallResult:1000,reduceFall:200,minExpPaybackRate:0.2}
 let tradingStrList = ref([]);
-function onClick() {
-  maplist.value = [1, 2, 3, 4, 5, 6];
+// 最大预期跌幅
+let maxFall = -0.35;
+
+// 计算所有可能的交易策略
+// 参数(本金,可买金额,可买金额)
+function calcAllTradingStraregy(cap, buy, sell) {
+  // 暂放结果
+  let tempResList = [];
+  // 买入1% - 100%
+  for (let index = 1; index < 101; index++) {
+    const rate = index / 100;
+    const title = "买入" + index + "%";
+    const tradingType = "buy";
+    // 最大预期跌幅结果
+    const maxFallResult =
+      (buy - buy * rate) + (sell + buy * rate) * (1 + maxFall);
+    // 最大跌幅结果减本金的差
+    const reduceFall = maxFallResult - cap;
+    const minExpPaybackRate = calcMinExpVol(cap, buy, sell, tradingType, rate);
+    const tempRes = {
+      title: title,
+      tradingType: tradingType,
+      rate: rate,
+      maxFallResult: maxFallResult,
+      reduceFall: reduceFall,
+      minExpPaybackRate: minExpPaybackRate,
+    };
+    tempResList.push(tempRes)
+  }
+  // 卖出1% - 100%
+  for (let index = 1; index < 101; index++) {
+    const rate = index / 100;
+    const title = "卖出" + index + "%";
+    const tradingType = "sell";
+    // 最大预期跌幅结果
+    const maxFallResult =
+      (buy + sell * rate) + (sell - sell * rate) * (1 + maxFall);
+    // 最大跌幅结果减本金的差
+    const reduceFall = maxFallResult - cap;
+    const minExpPaybackRate = calcMinExpVol(cap, buy, sell, tradingType, rate);
+    const tempRes = {
+      title: title,
+      tradingType: tradingType,
+      rate: rate,
+      maxFallResult: maxFallResult,
+      reduceFall: reduceFall,
+      minExpPaybackRate: minExpPaybackRate,
+    };
+    tempResList.push(tempRes)
+  }
+  // 不做操作
+  const hold_rate = 0;
+  const hold_title = "持仓不动";
+  const hold_tradingType = "hold";
+  // 最大预期跌幅结果
+  const hold_maxFallResult =
+    buy + sell * (1 + maxFall);
+  // 最大跌幅结果减本金的差
+  const hold_reduceFall = hold_maxFallResult - cap;
+  const hold_minExpPaybackRate = calcMinExpVol(cap, buy, sell, hold_tradingType, hold_rate);
+  const hold_tempRes = {
+    title: hold_title,
+    tradingType: hold_tradingType,
+    rate: hold_rate,
+    maxFallResult: hold_maxFallResult,
+    reduceFall: hold_reduceFall,
+    minExpPaybackRate: hold_minExpPaybackRate,
+  };
+  tempResList.push(hold_tempRes)
+}
+// 计算回本最小预期涨跌幅(涨跌幅后总资产等于本金)
+// 参数(本金,可买金额,可买金额,操作方向,操作比例)
+function calcMinExpVol(cap, buy, sell, tradingType, rate) {
+  let targetVol = 0;
+  // 投资后的总资产 = 本金
+  if (tradingType == "buy") {
+    // (buy - buy * rate) + (sell + buy * rate) * (1 + targetVol) = cap
+    targetVol = (cap - buy + buy * rate) / (sell + rate) - 1;
+  }
+  if (tradingType == "sell") {
+    // (buy + sell * rate) + (sell - sell * rate) * (1 + targetVol) = cap
+    targetVol = (cap - buy - sell * rate) / (sell - sell * rate) - 1;
+  }
+  if (tradingType == "hold") {
+    // buy + sell * (1 + targetVol) = cap
+    targetVol = (cap - buy) / sell - 1;
+  }
+  return targetVol;
 }
 </script>
 
@@ -151,12 +263,14 @@ body {
   border-radius: 5px;
   padding: 20px;
   color: #555;
+  margin-bottom: 10px;
 }
 .step-card-choose {
   background-color: #1890ff;
   border-radius: 5px;
   padding: 20px;
   color: #fff;
+  margin-bottom: 10px;
 }
 .text-sm {
   font-size: 12px;
